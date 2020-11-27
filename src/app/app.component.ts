958cf37770edc29b22f1b29132e9085bd5a8d0c7
farhan7272 <farhan7272@yahoo.in>
2020-11-26 21:23:29 -0600

    rxjs from of operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION,OnInit } from '@angular/core';
+import {of, from} from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,19 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit(){
+    of(2,3,4).subscribe(console.log);
+    from([5,6,7]).subscribe(console.log);
+
+    let apples = ['apple1','apple2','apple3'];
+
+    //of(...apples).subscribe(console.log);
+
+    of(...apples).subscribe(
+      apple => console.log(apple),
+      ()=> console.log('complete')
+    );
+  }
+
 }
